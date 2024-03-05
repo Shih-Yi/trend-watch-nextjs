8dfab235c245e7187379c325951f16179b3c5473
Shih-Yi <brvast@gmail.com>
2024-03-06 09:28:37 +1300

    add log and Error message

--- src/app/api/news/[id]/route.js
+++ src/app/api/news/[id]/route.js
@@ -17,16 +17,17 @@ export const PATCH = async (req, { params }) => {
   const client = new MongoClient(uri);
 
   try {
+    console.log('Post PATCH: Start connect')
     await client.connect();
     const collection = client.db('postToMongoDbLambda').collection('posts');
     const post = await collection.findOne({ _id: new ObjectId(id) });
-
+    console.log('Post PATCH: post load')
     if (!post) {
       return new Response("Post not found", { status: 500 })
     }
       
     if (!post.result) {
-      console.log('update---')
+      console.log('Post PATCH: if !post.result')
       const headerConfig = {
         headers: {
           'X-API-KEY': config.xApiKey
@@ -38,16 +39,16 @@ export const PATCH = async (req, { params }) => {
       } catch (error) {
         console.error('Error:', error.response ? error.response.data : error.message);
       }
-      console.log('body:1111----------', response);
+      console.log('Post PATCH: post finished', response);
       const summary = response.data.body
-      console.log('summary:', summary);
+      console.log('Post PATCH: post summary');
       const summaryResult = JSON.parse(summary)
       const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: summaryResult });
       return new Response(JSON.stringify(summaryResult), { status: 200 })
     }
     return new Response(JSON.stringify(post), { status: 200 })
   } catch (error) {
-    return new Response("Failed to fetch the post", { status: 500 })
+    return new Response(`Failed to fetch the post ${error.message}, Error: ${error}`, { status: 500 })
   } finally {
     await client.close();
   }
